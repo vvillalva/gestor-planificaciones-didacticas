83cf6d4afda362fcbf2cfbf211a9f74c66e4a8d0
vvillalva <victor.villalva0326@gmail.com>
2025-11-05 11:54:33 -0600

    fix: corregir regla de validación para el campo 'rol' en el registro de usuario

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,7 +33,7 @@ class RegisteredUserController extends Controller
         $request->validate([
             'nombre' => 'required|string|max:255',
             'apellido' => 'required|string|max:255',
-            'rol' => 'required|string|mas:255',
+            'rol' => 'required|string|max:255',
             'correo' => 'required|string|lowercase|email|max:255|unique:'.User::class,
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
